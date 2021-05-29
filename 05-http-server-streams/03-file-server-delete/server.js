e11f3ddfc8bf518b4adc2f6705adee8e077f94b7
m.studitskiy <m.studitskiy@rambler-co.ru>
2021-05-29 14:28:04 +0300

    resolve task 5-3

--- 05-http-server-streams/03-file-server-delete/server.js
+++ 05-http-server-streams/03-file-server-delete/server.js
@@ -1,16 +1,36 @@
-const url = require('url');
 const http = require('http');
 const path = require('path');
+const { unlink } = require('fs');
 
 const server = new http.Server();
 
 server.on('request', (req, res) => {
-  const pathname = url.parse(req.url).pathname.slice(1);
+  const url = new URL(req.url, `http://${req.headers.host}`);
+  const pathname = url.pathname.slice(1);
 
   const filepath = path.join(__dirname, 'files', pathname);
 
   switch (req.method) {
     case 'DELETE':
+      if (!pathname) {
+        res.statusCode = 400;
+        res.end('Should be name of file in request');
+      }
+
+      if (pathname.includes('/')) {
+        res.statusCode = 400;
+        res.end('No nested files');
+      }
+
+      unlink(filepath, (err) => {
+        if (err) {
+          res.statusCode = 404;
+          res.end('Not found');
+        }
+
+        res.statusCode = 200;
+        res.end(`${pathname} has been deleted`);
+      });
 
       break;
 
