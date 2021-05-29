54a567f8b8580ef58a7c1c53f36cb0b6781291eb
m.studitskiy <m.studitskiy@rambler-co.ru>
2021-05-29 13:28:08 +0300

    resolve task 5-2 (partial)

--- 05-http-server-streams/02-file-server-post/server.js
+++ 05-http-server-streams/02-file-server-post/server.js
@@ -1,16 +1,62 @@
-const url = require('url');
 const http = require('http');
 const path = require('path');
+const { createWriteStream, unlink } = require('fs');
+const LimitSizeStream = require('./LimitSizeStream');
+const { pipeline } = require('stream');
 
 const server = new http.Server();
 
 server.on('request', (req, res) => {
-  const pathname = url.parse(req.url).pathname.slice(1);
-
+  const url = new URL(req.url, `http://${req.headers.host}`);
+  const pathname = url.pathname.slice(1);
   const filepath = path.join(__dirname, 'files', pathname);
 
   switch (req.method) {
     case 'POST':
+      if (pathname.includes('/')) {
+        res.statusCode = 400;
+        res.end('No nested files');
+        return;
+      }
+
+      const writeStream = createWriteStream(filepath, { flags: 'wx' });
+      const transformStream = new LimitSizeStream({
+        limit: 1000000,
+        readableObjectMode: false,
+      });
+
+      pipeline(req, transformStream, writeStream, (e) => {
+        if (e) {
+          switch (e.code) {
+            case 'EEXIST':
+              res.statusCode = 409;
+              res.end('Already exist');
+              break;
+            case 'ERR_STREAM_PREMATURE_CLOSE':
+              unlink(filepath, () => {
+                res.statusCode = 500;
+                res.end('Client aborted connection');
+              });
+              break;
+            case 'LIMIT_EXCEEDED':
+              writeStream.destroy();
+              transformStream.destroy();
+              unlink(filepath, () => {
+                res.statusCode = 413;
+                res.end('File size limit reached');
+              });
+
+              break;
+
+            default:
+              res.statusCode = 501;
+              res.end('Not implemented');
+          }
+        } else {
+          res.statusCode = 201;
+          res.end();
+        }
+      });
 
       break;
 
