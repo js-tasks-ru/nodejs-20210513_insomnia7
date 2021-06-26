955a80db2784b1094d5536a2e01dbc5ae0ac2afa
m.studitskiy <m.studitskiy@rambler-co.ru>
2021-06-26 09:00:26 +0300

    add mustBeAuthenticated file

--- 08-authentication/03-sessions/controllers/me.js
+++ 08-authentication/03-sessions/controllers/me.js
@@ -1,11 +1,4 @@
 module.exports.me = async function me(ctx, next) {
-  if (!ctx.user) {
-    ctx.status = 401
-    ctx.body = {error: 'Пользователь не залогинен'}
-
-    return
-  }
-
   ctx.body = {
     email: ctx.user.email,
     displayName: ctx.user.displayName,
